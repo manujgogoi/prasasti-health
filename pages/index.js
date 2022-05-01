cf54d0833bac0c211aa9ce15a250ab22fe9c47a6
Manuj Gogoi <manujgogoi@gmail.com>
2022-05-01 09:14:20 +0530

    index.js page update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className="text-5xl font-bold">Hello NextJS</h1>
+    <>
+    <h1 className="text-5xl font-bold">prasastihealth.in</h1>
+    <h2>Website is migrating...</h2>
+    </>
   )
 }
